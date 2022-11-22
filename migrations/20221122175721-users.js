1e76cc5bb4ef755dd5ca31b22e6e340a2971ec12
Marcelo Schmidt <gmmarceloo@gmail.com>
2022-11-22 15:16:59 -0300

    Initial commit

---
+++ migrations/20221122175721-users.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users")
+  }
+};
